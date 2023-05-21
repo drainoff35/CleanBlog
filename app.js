d0b0a8395475c50c97e2b0b3cf610e4823e2a7b3
Kutay <81999664+drainoff35@users.noreply.github.com>
2023-05-21 18:30:42 +0300

    ejs, page routing entegrated

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
 
 const port = 3000;
 
-app.get('/', (req, res) =>{
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog);
-})
+//Middlewares
+app.use(express.static('public'))
 
-app.listen(port, ()=>{
-    console.log(`Uygulama ${port} portunda başlatıldı..`);
-} )
+const myLogger = (req,res,next) => {
+  console.log("Middleware logging 1");
+  next();
+}
+const myLogger2 = (req,res,next) => {
+  console.log("Middleware logging 2");
+  next();
+}
+
+app.use(myLogger);
+app.use(myLogger2);
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.listen(port, () => {
+  console.log(`Uygulama ${port} portunda başlatıldı..`);
+});
